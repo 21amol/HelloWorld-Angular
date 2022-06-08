ba419a8c32e10ac12d6897734aa814e03f2fece6
Amol Ghotale <ghotaleamol@gmail.com>
2022-06-08 15:52:22 +0530

    ADDED: Validation for Name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,23 @@ export class AppComponent {
   title = 'Hello from BridgeLabz';
   imgUrl ="./assets/BridgeLabz-logo.jpg"; 
   url = "https://www.bridgelabz.com";
-  userName: string = " Amol";
+  userName: string = "";
+  nameError: string = "";
 
 
 onclick($event) {
   console.log("Save Button is Clicked", $event);
   window.open(this.url, "_blank");
+ }
+
+onInput($event) {
+  console.log("Change Event Occured", $event.data);
+  var nameRegex = RegExp('^[A-Z]{1}[a-z]{1,}$');
+  if (nameRegex.test(this.userName)) {
+    this.nameError = "";
+    return;
+  }
+  this.nameError = "Invalid Name!!!";
  }
 }
 
